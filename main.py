e54dcf26b2ef031b6d2d7d5438f908a8dc7302e7
Gamer303HD <58631771+Gamer303HD@users.noreply.github.com>
2024-07-03 12:38:15 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,149 @@
+import os
+import tkinter
+
+def clear():
+    os.system("cls")
+    #os.system("clear")
+def skip():
+    for x in range(6):
+        print(" ")
+def skipnumber(number):
+    for x in range(number):
+        print(" ")
+def caps():
+    skipnumber(2)
+    print("███╗░░░███╗░█████╗░████████╗░█████╗░██████╗░  ██████╗░░█████╗░░██████╗██╗████████╗██╗░█████╗░███╗░░██╗")
+    print("████╗░████║██╔══██╗╚══██╔══╝██╔══██╗██╔══██╗  ██╔══██╗██╔══██╗██╔════╝██║╚══██╔══╝██║██╔══██╗████╗░██║")
+    print("██╔████╔██║██║░░██║░░░██║░░░██║░░██║██████╔╝  ██████╔╝██║░░██║╚█████╗░██║░░░██║░░░██║██║░░██║██╔██╗██║")
+    print("██║╚██╔╝██║██║░░██║░░░██║░░░██║░░██║██╔══██╗  ██╔═══╝░██║░░██║░╚═══██╗██║░░░██║░░░██║██║░░██║██║╚████║")
+    print("██║░╚═╝░██║╚█████╔╝░░░██║░░░╚█████╔╝██║░░██║  ██║░░░░░╚█████╔╝██████╔╝██║░░░██║░░░██║╚█████╔╝██║░╚███║")
+    print("╚═╝░░░░░╚═╝░╚════╝░░░░╚═╝░░░░╚════╝░╚═╝░░╚═╝  ╚═╝░░░░░░╚════╝░╚═════╝░╚═╝░░░╚═╝░░░╚═╝░╚════╝░╚═╝░░╚══╝")
+    for x in range(6):
+        print(" ")
+
+clear()
+caps()
+print("Enter CAN ID 1 bis 9")
+canbyid = input()
+print("Select: " + canbyid)
+clear()
+caps()
+clear()
+caps()
+print("Enter Speed von 1 bis 3000")
+speedid = input()
+print("Select: " + speedid)
+clear()
+caps()
+print("Enter Acceralition 1 bis 255")
+acceralitionid = input()
+print("Select: " + acceralitionid)
+clear()
+caps()
+print("Enter Position 1 bis 16777215")
+positionid = input()
+print("Select: " + positionid)
+clear()
+speedidhex = hex(int(speedid))[2:]
+speedidnumbers = len(str(speedidhex))
+if speedidnumbers == 1:
+    speedidenter = "00 0" + speedidhex
+elif speedidnumbers == 2:
+    speedidenter = "00 " + speedidhex
+elif speedidnumbers == 3:
+    speedidenter = "0" + speedidhex[:1] + " " + speedidhex[1:]
+elif speedidnumbers == 4:
+    speedidenter = speedidhex
+
+acceralitionidhex = hex(int(acceralitionid))[2:]
+acceralitionidnumbers = len(str(acceralitionidhex))
+if acceralitionidnumbers == 1:
+    acceralitionidenter = "0" + acceralitionidhex
+elif acceralitionidnumbers == 2:
+    acceralitionidenter = acceralitionidhex
+
+
+positionidhex = hex(int(positionid))[2:]
+positionidnumbers = len(str(positionidhex))
+if positionidnumbers == 1:
+    positionidenter = "00000" + str(positionidhex)[:1]
+    positionidenter1 = ' '.join(positionidenter[i:i+2] for i in range(0, len(positionidenter), 2))
+elif positionidnumbers == 2:
+    positionidenter = "0000" + str(positionidhex)[:2]
+    positionidenter1 = ' '.join(positionidenter[i:i+2] for i in range(0, len(positionidenter), 2))
+elif positionidnumbers == 3:
+    positionidenter = "000" + str(positionidhex)[:3]
+    positionidenter1 = ' '.join(positionidenter[i:i+2] for i in range(0, len(positionidenter), 2))
+elif positionidnumbers == 4:
+    positionidenter = "00" + str(positionidhex)[:4]
+    positionidenter1 = ' '.join(positionidenter[i:i+2] for i in range(0, len(positionidenter), 2))
+elif positionidnumbers == 5:
+    positionidenter = "0" + str(positionidhex)[:5]
+    positionidenter1 = ' '.join(positionidenter[i:i+2] for i in range(0, len(positionidenter), 2))
+elif positionidnumbers == 6:
+    positionidenter = str(positionidhex)
+    positionidenter1 = ' '.join(positionidenter[i:i+2] for i in range(0, len(positionidenter), 2))
+
+
+modeidhex = hex(245)[2:]
+canbyidhex = hex(int(canbyid))
+canbyidoutput = canbyidhex.replace('x', '')
+
+
+
+check = int(canbyid) + int(speedid) + int(acceralitionid) + int(positionid) + 245
+checkhex = hex(check)[2:]
+checkhexoutput = hex(check)
+
+checkidnumber = len(str(checkhex))
+if checkidnumber == 1:
+    checkhex = "0" + checkhex
+elif checkidnumber == 3:
+    checkhex = checkhex[-2:]  #
+elif checkidnumber == 4:
+    checkhex = checkhex[-3:]
+elif checkidnumber == 5:
+    checkhex = checkhex[-4:]
+elif checkidnumber == 6:
+    checkhex = checkhex[-8:]  #
+elif checkidnumber == 7:
+    checkhex = checkhex[-6:]
+elif checkidnumber == 8:
+    checkhex = checkhex[-7:]   
+
+caps()
+#last 2 of result so example = 2B5 --> B5
+print("======================================================")
+print(canbyidoutput + " " + modeidhex + " " + speedidenter + " " + acceralitionidenter + " " + positionidenter1 + " " + checkhex +"                  CRC: " + checkhexoutput)
+print("======================================================")
+
+def append_to_file_if_needed(text, file_name, line_length):
+    # Versuchen, die Datei im Lesemodus zu öffnen, um den bestehenden Inhalt zu lesen
+    try:
+        with open(file_name, 'r', encoding='utf-8') as file:
+            existing_content = file.read()
+    except FileNotFoundError:
+        existing_content = ""
+
+    # Öffnen der Datei im Anhängemodus ('a') zum Hinzufügen von Text
+    with open(file_name, 'a', encoding='utf-8') as file:
+        # Überprüfen, ob die Datei bereits Text enthält
+        if existing_content:
+            file.write('\n')  # Neue Zeile hinzufügen, falls bereits Text vorhanden ist
+
+        # Text in Teile der Länge line_length aufteilen und hinzufügen
+        for i in range(0, len(text), line_length):
+            file.write(text[i:i+line_length] + '')
+
+text = canbyidoutput + " " + modeidhex + " " + speedidenter + " " + acceralitionidenter + " " + positionidenter1 + " " + checkhex + "  CRC: " + checkhexoutput
+file_name = "log.txt"
+line_length = 50
+
+append_to_file_if_needed(text, file_name, line_length)
+print(f"Text wurde erfolgreich in die Datei {file_name} geschrieben.")
+
+skip()
+enter = input("Press Enter ")
+
+
+
